18cc1cb9bca2113a44e159111120b4e546660bed
pcaceros-mc <pcaceros@mangochango.com>
2023-11-20 19:04:01 -0600

    Binary search

--- search/binary-search.js
+++ search/binary-search.js
@@ -2,25 +2,36 @@
 Given a sorted array of 'n' elements and a target element 't', find the index of 't' 
 in the array, return -1 if the target element is not found. 
 */
+/*
+     L           R
+  [ -5, 2, 4, 6, 10 ]
+  <  0, 1, 2, 3, 4  >
+              L   R
+  [ -5, 2, 4, 6, 10 ]
+  <  0, 1, 2, 3, 4  >
 
-// [ -5, 2, 4, 6 , 10 ]
-// [ -5, 2, 4, 6 , 10 ]
+*/
 
-function binarySearch(arr, t) {
-
-  let rightIdx = arr.length;
+function binarySearch(arr, target) {
+  let rightIdx = arr.length - 1;
   let leftIdx = 0;
 
-  let middle = 
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.round((leftIdx + rightIdx) / 2);
 
+    if (target === arr[middleIdx]) {
+      return middleIdx;
+    }
 
-
-  
-  };
-  return leftIdx;
+    if (target < arr[middleIdx]) {
+      rightIdx = middleIdx - 1;
+    } else {
+      leftIdx = middleIdx + 1;
+    }
+  }
+  return -1;
 }
 
 console.log(binarySearch([-5, 2, 4, 6, 10], 6));
-
-// console.log(binarySearch([-5, 2, 4, 6, 10], 10));
-// console.log(binarySearch([-5, 2, 4, 6, 10, 20, 23, 102], 20));
+console.log(binarySearch([-5, 2, 4, 6, 10], 10));
+console.log(binarySearch([-5, 2, 4, 6, 10, 20, 30], 4));
